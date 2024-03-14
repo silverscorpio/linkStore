e8ac669e1e1dd4d4d48296a70ac5c23a2f73a281
silverscorpio <rajat.newtoneinstein@gmail.com>
2024-03-14 23:25:39 +0100

    templates, urls store, views home and index added, more work

--- store/urls.py
+++ store/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+from store import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("home/", views.home, name="home"),
+]
--- store/views.py
+++ store/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, "store/index.html")
+
+
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, "store/home.html")
+    return HttpResponse("Sorry, not logged in")
