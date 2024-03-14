7ab0fe3c5f0d581a1bdbcb3026756f2c876b393f
silverscorpio <rajat.newtoneinstein@gmail.com>
2024-03-14 22:29:46 +0100

    store app admin register

--- store/admin.py
+++ store/admin.py
@@ -1,1 +1,6 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Topic, Tag, Link
+
+admin.site.register(Topic)
+admin.site.register(Tag)
+admin.site.register(Link)
