a9187191a0b4b601d180eedb2befa6ac60f87371
silverscorpio <rajat.newtoneinstein@gmail.com>
2024-03-17 01:02:33 +0100

    app name added to urls

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from store import views
 
+app_name = "store"
+
 urlpatterns = [
     path("", views.landing, name="landing"),
     path("home/", views.home, name="home"),
