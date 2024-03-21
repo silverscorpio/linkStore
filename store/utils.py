4c19bd5d90a0b4927bde73a74d32be1ddc676fdf
silverscorpio <rajat.newtoneinstein@gmail.com>
2024-03-21 16:55:01 +0100

    added headers to get

--- store/utils.py
+++ store/utils.py
@@ -1,9 +1,10 @@
+from linkStore.settings import HEADERS
 from bs4 import BeautifulSoup
 import httpx
 
 
 def get_link_title(url: str) -> str:
-    r = httpx.get(url)
+    r = httpx.get(url, headers=HEADERS)
     if r.status_code == 200:
         soup = BeautifulSoup(r.content, features="html.parser")
         if soup.title:
