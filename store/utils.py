7fd9159da085574a6578c085c7653db96854ba69
silverscorpio <rajat.newtoneinstein@gmail.com>
2024-03-21 10:35:54 +0100

    check for title tag in soup

--- store/utils.py
+++ store/utils.py
@@ -6,6 +6,6 @@ def get_link_title(url: str) -> str:
     r = httpx.get(url)
     if r.status_code == 200:
         soup = BeautifulSoup(r.content, features="html.parser")
-        if soup.head:
-            return soup.head.title.string
+        if soup.title:
+            return str(soup.title.string)
     return ""
