eec16bccba5ebd353f0f06e7be6a50dbdaedb715
silverscorpio <rajat.newtoneinstein@gmail.com>
2024-06-13 23:57:21 +0200

    ruffed all

--- linkStore/wsgi.py
+++ linkStore/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'linkStore.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "linkStore.settings")
 
 application = get_wsgi_application()
