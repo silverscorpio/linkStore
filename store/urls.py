e7d06d272cd30dd09b1065268f9b3a14e89308a5
silverscorpio <rajat.newtoneinstein@gmail.com>
2024-06-17 02:38:51 +0200

    get added to link detail view, redundant path removed, working, cookies clear

--- store/urls.py
+++ store/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path("links/<int:pk>/", views.LinkDetailView.as_view(), name="link-detail"),
     path("topics/<int:pk>/", views.TopicDetailView.as_view(), name="topic-detail"),
     path("topics/<int:pk>/", views.TagDetailView.as_view(), name="tag-detail"),
-    path("links/<int:pk>/", views.LinkDetailView.as_view(), name="link-read-marked"),
 ]
