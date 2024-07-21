55fcbc1d27b07fb020b6cf84b87438d1cc237ff9
silverscorpio <rajat.newtoneinstein@gmail.com>
2024-07-21 20:01:15 +0200

    added owner to topic and tag models, tied it to the user

--- store/models.py
+++ store/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from store.utils import get_link_title
+from django.contrib.auth.models import User
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=20)
     creation_date = models.DateField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name.lower()
@@ -16,6 +18,7 @@ class Tag(models.Model):
 class Topic(models.Model):
     name = models.CharField(max_length=50)
     creation_date = models.DateField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name.title()
