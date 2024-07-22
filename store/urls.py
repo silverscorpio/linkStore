1682300a03fcc14d5f23ba45753f9ac311f8b55b
silverscorpio <rajat.newtoneinstein@gmail.com>
2024-07-22 04:18:32 +0200

    topic detail and update view with model forms added, templates updated

--- store/urls.py
+++ store/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("links/", views.LinkListView.as_view(), name="links"),
     path("topics/", views.TopicListView.as_view(), name="topics"),
     path("tags/", views.TagListView.as_view(), name="tags"),
-    path("links/<int:pk>/", views.LinkDetailView.as_view(), name="link-detail"),
-    path("topics/<int:pk>/", views.TopicDetailView.as_view(), name="topic-detail"),
-    path("topics/<int:pk>/", views.TagDetailView.as_view(), name="tag-detail"),
+    path("links/<int:pk>/", views.LinkDetailView.as_view(), name="link-update"),
+    path("topics/<int:pk>/", views.TopicUpdateView.as_view(), name="topic-update"),
+    path("topics/<int:pk>/", views.TagDetailView.as_view(), name="tag-update"),
 ]
--- store/views.py
+++ store/views.py
@@ -3,29 +3,30 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from .models import Link, Topic, Tag
+from .forms import TopicForm
 
 
 def landing(request):
     # TODO could use directly the links page (LinkListView) but maybe a different landing page?
     if request.user.is_authenticated:
-        links = Link.objects.filter(topic__owner=request.user).order_by("save_date")
+        links = Link.objects.filter(topic__owner=request.user).order_by("-saved_on")
         return render(request, "store/link_list.html", context={"link_list": links})
     return render(request, "store/landing.html")
 
 
 class LinkListView(generic.ListView):
     def get_queryset(self):
-        return Link.objects.filter(topic__owner=self.request.user).order_by("save_date")
+        return Link.objects.filter(topic__owner=self.request.user).order_by("-saved_on")
 
 
 class TopicListView(generic.ListView):
     def get_queryset(self):
-        return Topic.objects.filter(owner=self.request.user).order_by("creation_date")
+        return Topic.objects.filter(owner=self.request.user).order_by("-updated_on")
 
 
 class TagListView(generic.ListView):
     def get_queryset(self):
-        return Tag.objects.filter(owner=self.request.user).order_by("creation_date")
+        return Tag.objects.filter(owner=self.request.user).order_by("-updated_on")
 
 
 class LinkDetailView(generic.DetailView):
@@ -46,8 +47,15 @@ class LinkDetailView(generic.DetailView):
         return HttpResponseRedirect(reverse("store:links"))
 
 
-class TopicDetailView(generic.DetailView):
+class TopicUpdateView(generic.edit.UpdateView):
     model = Topic
+    form_class = TopicForm
+
+    # TODO maybe abstract the nav bar part into base and remove separate base detail and base list
+    template_name = "store/topic_detail.html"
+    # context object name refers to the model data inside the context object
+    # the form object inside the context object is called 'form'
+    context_object_name = "topic_detail"
 
 
 class TagDetailView(generic.DetailView):
