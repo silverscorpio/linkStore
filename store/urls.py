04e47eb8b6f29c987130f346a54799e0d6313574
silverscorpio <rajat.newtoneinstein@gmail.com>
2024-07-27 09:41:34 +0200

    corrected view name, reformat

--- store/urls.py
+++ store/urls.py
@@ -5,22 +5,23 @@ app_name = "store"
 
 urlpatterns = [
     path("", views.landing, name="landing"),
-    # list views
+    # List views
     path("links/", views.LinkListView.as_view(), name="links"),
     path("topics/", views.TopicListView.as_view(), name="topics"),
     path("tags/", views.TagListView.as_view(), name="tags"),
-    path("links/<int:pk>/", views.LinkDetailView.as_view(), name="link-update"),
-    # update
+    # Detail views
+    path("links/<int:pk>/", views.LinkDetailView.as_view(), name="link-detail"),
+    # Update
     path("links/<int:pk>/update/", views.LinkUpdateView.as_view(), name="link-update"),
     path(
         "topics/<int:pk>/update/", views.TopicUpdateView.as_view(), name="topic-update"
     ),
     path("tags/<int:pk>/update/", views.TagUpdateView.as_view(), name="tag-update"),
-    # add
+    # Add
     path("links/add/", views.LinkCreateView.as_view(), name="link-add"),
     path("topics/add/", views.TopicCreateView.as_view(), name="topic-add"),
     path("tags/add/", views.TagCreateView.as_view(), name="tag-add"),
-    # delete
+    # Delete
     path("links/<int:pk>/delete/", views.LinkDeleteView.as_view(), name="link-delete"),
     path(
         "topics/<int:pk>/delete/", views.TopicDeleteView.as_view(), name="topic-delete"
