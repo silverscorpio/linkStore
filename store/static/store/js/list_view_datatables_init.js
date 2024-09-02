0328a3612666380470f0d4c39c6b10cd45c8d158
silverscorpio <rajat.newtoneinstein@gmail.com>
2024-09-02 02:32:55 +0200

    formatting for datatables, luxon datetime format, css padding for paging and info

--- store/static/store/js/list_view_datatables_init.js
+++ store/static/store/js/list_view_datatables_init.js
@@ -1,2 +1,15 @@
 // for list views (table)
-let table = new DataTable('#listTable')
+DataTable.datetime('d MMM yy');
+let table = new DataTable('#listTable', {
+    lengthMenu: [10, 25, 50, {label: 'All', value: -1}],
+    layout: {
+        topStart: 'pageLength',
+        topEnd: {
+            search: {
+                placeholder: 'Search LinkStore',
+            }
+        },
+        bottomStart: 'info',
+        bottomEnd: 'paging'
+    }
+})
